6a83eb3db39dace55af6c7dd450a63fb032973eb
pmcl777888999@gmail.com <pmcl777888999@gmail.com>
2023-03-10 08:25:37 -0300

    "coso otra vez"

--- main.cpp
+++ main.cpp
@@ -1,9 +1,5 @@
-<<<<<<< Updated upstream
+
 //esto es una prueba del cpp
-<<<<<<< HEAD
-=======
 //mi wea 
->>>>>>> Stashed changes
-=======
 //segunda prueba
->>>>>>> d6becfcf7bd8fe847c6d47e5a9f447b49ebc2786
+
